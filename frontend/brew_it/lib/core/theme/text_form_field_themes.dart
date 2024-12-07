e3b895af21cce3b32bec3383914095dc8acbb388
Angelika Ostrowska <01178572@pw.edu.pl>
2024-12-07 23:29:45 +0100

    Improved text form field style

---
+++ frontend/brew_it/lib/core/theme/text_form_field_themes.dart
@@ -0,0 +1,14 @@
+import 'package:brew_it/core/theme/colors.dart';
+import 'package:brew_it/core/theme/text_themes.dart';
+import 'package:flutter/material.dart';
+
+InputDecorationTheme baseTextFormFieldTheme = InputDecorationTheme(
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+    contentPadding: const EdgeInsets.symmetric(
+      vertical: 13,
+      horizontal: 13,
+    ),
+    fillColor: textLightColor,
+    filled: true,
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    labelStyle: baseTextTheme.titleLarge);
